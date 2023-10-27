e4d7c64b2ac3fd955566520964ce4ae84a661904
kxiside <swendside@gmail.com>
2023-10-26 19:22:14 -0700

    Parallax layers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
-import { Container, Card } from "react-bootstrap"
+
 import { Link } from "react-router-dom"
 import "./Style/Home.css"
+import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 
 const Home = (props) => {
 	 const { msgAlert, user } = props
 
 	return (
-		<>
-		
-		</>
+		<div>
+			<Parallax pages ={2} style={{ top: '0', left: '0' }} class="animation">
+				<ParallaxLayer offset={0} speed={2.5}>
+					<div class="animation_layer parallax" id=""></div>
+				</ParallaxLayer>
+				<ParallaxLayer offset={0} speed={2.5}>
+					<div class="animation_layer parallax" id=""></div>
+				</ParallaxLayer>
+			</Parallax>
+		</div>
 
 	)
 }
